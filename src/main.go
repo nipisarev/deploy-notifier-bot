4f6e881b1e55db2a718b9907bdada52475d98829
Nickolay Pisarev <n.pisarev@hostcomm.ru>
2017-05-03 19:03:29 +0300

    add routing

--- src/main.go
+++ src/main.go
@@ -5,12 +5,17 @@ import (
 	"html"
 	"log"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.RequestURI))
-	})
+	router := httprouter.New()
+	router.GET("/*all", Index)
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.RequestURI))
 }
